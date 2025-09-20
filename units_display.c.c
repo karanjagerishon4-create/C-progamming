d7278c4f170964c5717accfa5f2ecfb724d010a3
karanjagerishon4-create <karanjagerishon4@gmail.com>
2025-09-20 11:17:36 +0300

    Add files via upload

---
+++ units_display.c.c
@@ -0,0 +1,29 @@
+/*
+Name:Gerishon Kamau Karanja
+Reg:PA106/G/29211/25
+Description:Units display
+*/
+
+#include<stdio.h>
+
+//main function
+
+int main(){
+	//display of units
+	printf("SPC2101 Introduction to programming and problem solving");
+	
+	printf("\n SPM2100 Discrete mathematics");
+	
+	printf("\n SPI2102 Computer systems & organisation");
+	
+	printf("\n SPM2104 mathematics for science");
+	
+	printf("\n SSE2101 Fundamentals of system analysis & design");
+	
+	printf("\n UCU2105 Fundamentals of ICT");
+	
+	printf("\n UCU2101 Communications & information literacy skills");
+	
+	return 0;
+	
+}
